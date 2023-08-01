eeef5fa6ac1afd7d7d0158660074d722d0094576
zhoooou <52wdwpf@gmail.com>
2023-08-02 02:45:55 +0800

    modify the .zshrc and the init.sh to alias the rivscv64 tool for work

---
+++ Arc/os/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void print(char * a){
+  printf("%s", a);
+}
+int main() {
+	print("666");
+  return 0;
+}
--- init_app.sh
+++ init_app.sh
@@ -1,2 +1,2 @@
-sudo pacman -S yay git vim neovim gcc gdb man man-pages sdl2 sdl2_image
+sudo pacman -S yay git vim neovim gcc gdb man man-pages sdl2 sdl2_image riscv64-linux-gnu-binutils riscv64-linux-gnu-gdb riscv64-linux-gnu-gcc
 yay -S wps-office
