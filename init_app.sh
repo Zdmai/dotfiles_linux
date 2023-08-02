3c519cf102ae89905fdca631f06f7b3b92479dfc
zhoooou <52wdwpf@gmail.com>
2023-08-02 23:22:36 +0800

    modify the .sh file

--- init_app.sh
+++ init_app.sh
@@ -1,2 +1,2 @@
-sudo pacman -S yay git vim neovim gcc gdb man man-pages sdl2 sdl2_image riscv64-linux-gnu-binutils riscv64-linux-gnu-gdb riscv64-linux-gnu-gcc
+sudo pacman -S yay git vim neovim gcc gdb man man-pages sdl2 sdl2_image riscv64-linux-gnu-binutils riscv64-linux-gnu-gdb riscv64-linux-gnu-gcc llvm linux-firmware-qlogic
 yay -S wps-office
